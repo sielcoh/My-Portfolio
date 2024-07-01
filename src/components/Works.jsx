fd6c2806fa793dcaf2375627b03815032e29ea39
sielcoh <sielcoh@gmail.com>
2024-07-01 18:11:10 +0300

    cosmetics to the image projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -13,7 +13,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt options={{ max: 45, scale: 1, speed: 450 }} className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'>
         <div className="relative w-full h-[230px]">
-          <img src={image} alt={image} className="w-full h-full object-cover rounded-2xl" />
+          <img src={image} alt={image} className="h-[232px] w-[322px] rounded-2xl" />
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div onClick={()=>window.open(source_code_link,"_blank")} className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
               <img src={github} alt={github} className="w-1/2 h-1/2 object-contain"/>
